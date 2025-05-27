192fcf4a198100d9899d21cdecc5018ab3dfdea3
DmytroLysachenko <dlysachenko98@gmail.com>
2025-05-27 09:58:21 +0200

    Added utils and sidebar

---
+++ components/MobileSidebar.tsx
@@ -0,0 +1,51 @@
+//@ts-nocheck
+
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import React from "react";
+import { Link } from "react-router";
+import NavItems from "./NavItems";
+
+const MobileSidebar = () => {
+  let sidebar: SidebarComponent;
+
+  const toggleSidebar = () => {
+    sidebar.toggle();
+  };
+
+  return (
+    <div className="mobile-sidebar wrapper">
+      <header>
+        <Link to="/">
+          <img
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            className="size-[30px]"
+          />
+
+          <h1>TravelApp</h1>
+        </Link>
+
+        <button onClick={toggleSidebar}>
+          <img
+            src="/assets/icons/menu.svg"
+            alt="menu"
+            className="size-[30px]"
+          />
+        </button>
+      </header>
+
+      <SidebarComponent
+        width={170}
+        ref={(node) => (sidebar = node)}
+        created={() => sidebar.hide()}
+        closeOnDocumentClick
+        showBackdrop
+        type="over"
+      >
+        <NavItems handleClick={toggleSidebar} />
+      </SidebarComponent>
+    </div>
+  );
+};
+
+export default MobileSidebar;
