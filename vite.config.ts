a663d0a74be9721a35ff74c09cee3c0e4b2e9163
DmytroLysachenko <dlysachenko98@gmail.com>
2025-06-06 10:30:00 +0200

    Added readme to the project

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 const sentryConfig: SentryReactRouterBuildOptions = {
   org: "my-own-company-c1",
   project: "javascript-react",
-
-  // An auth token is required for uploading source maps.
   authToken:
     "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",
-  // ...
 };
 
 export default defineConfig((config) => {
