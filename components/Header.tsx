3e4dff829f2b61f9762dabda66d67c34cdf31cbc
DmytroLysachenko <dlysachenko98@gmail.com>
2025-06-08 23:18:24 +0200

    Added trips page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,16 @@
-import { useLocation } from "react-router";
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
+import { Link, useLocation } from "react-router";
 
 import { cn } from "~/lib/utils";
 
 interface Props {
   title: string;
   description: string;
+  ctaText?: string;
+  ctaUrl?: string;
 }
 
-const Header = ({ title, description }: Props) => {
+const Header = ({ title, description, ctaText, ctaUrl }: Props) => {
   const location = useLocation();
 
   return (
@@ -32,6 +35,24 @@ const Header = ({ title, description }: Props) => {
           {description}
         </p>
       </article>
+      {ctaText && ctaUrl && (
+        <Link
+          to={ctaUrl}
+          className="inline-block mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+        >
+          <ButtonComponent
+            type="button"
+            className="button-class !h-11 !w-full md:w-[240px]"
+          >
+            <img
+              src="/assets/icons/plus.svg"
+              alt="plus"
+              className="size-5"
+            />
+            <span className="p-16-semibold text-white">{ctaText}</span>
+          </ButtonComponent>
+        </Link>
+      )}
     </header>
   );
 };
